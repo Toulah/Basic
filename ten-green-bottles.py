4539dad6969258d5b63d6e808ab3ce9a0a7de64a
Toulah <99089744+Toulah@users.noreply.github.com>
2022-02-06 23:28:15 -0500

    wrote my ten green bottle poem

--- ten-green-bottles.py
+++ ten-green-bottles.py
@@ -1,1 +1,76 @@
 
+#Poem: 10 green bottles hanging on a wall Source:https://allnurseryrhymes.com/ten-green-bottles/
+number = 10
+line1_2 = " green bottles hanging on the wall"
+line3 = "And if one green bottle should accidentally fall,"
+line4_1 = "There’ll be "
+line4_2 = " green bottles hanging on the wall."
+
+number = 10
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
+
+print()
+number = 9
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
+
+print()
+number = 8
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
+
+print()
+number = 7
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
+
+print()
+number = 6
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
+
+print()
+number = 5
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
+
+print()
+number = 4
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
+
+print()
+number = 3
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
+
+print()
+number = 2
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
+
+print()
+number = 1
+print(str(number) + line1_2)
+print(str(number) + line1_2)
+print(line3)
+print(line4_1 + str(number-1) + line4_2)
