85f0bd3b0f3ae5efa3079c599a0510bd4bec48ca
Toulah <99089744+Toulah@users.noreply.github.com>
2022-02-07 21:02:47 -0500

    changed numbers to words

--- ten-green-bottles.py
+++ ten-green-bottles.py
@@ -1,76 +1,84 @@
+# Poem: 10 green bottles hanging on the wall Source:https://allnurseryrhymes.com/ten-green-bottles/
+print("Ten Green Bottles Hanging on the Wall")
+print()
 
-#Poem: 10 green bottles hanging on a wall Source:https://allnurseryrhymes.com/ten-green-bottles/
 number = 10
 line1_2 = " green bottles hanging on the wall"
-line3 = "And if one green bottle should accidentally fall,"
-line4_1 = "There’ll be "
+line3 = "and if one green bottle should accidentally fall,"
+line4_1 = "there’ll be "
 line4_2 = " green bottles hanging on the wall."
 
+# Numbers in the poem should all be in words
+# Used list to convert numbers to word
+
+
+num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eighth", "nine", "ten"]
+
 number = 10
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
 
 print()
 number = 9
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
 
 print()
 number = 8
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
 
 print()
 number = 7
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
 
 print()
 number = 6
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
 
 print()
 number = 5
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
 
 print()
 number = 4
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
 
 print()
 number = 3
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
 
 print()
 number = 2
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
 
 print()
 number = 1
-print(str(number) + line1_2)
-print(str(number) + line1_2)
+print(num[number] + line1_2)
+print(num[number] + line1_2)
 print(line3)
-print(line4_1 + str(number-1) + line4_2)
+print(line4_1 + num[number-1] + line4_2)
