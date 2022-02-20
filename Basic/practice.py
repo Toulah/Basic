e0393656bd6f237102bfd41d80a7053c29e1dc2f
Toulah <99089744+Toulah@users.noreply.github.com>
2022-02-20 15:06:12 -0500

    refactoring

---
+++ Basic/practice.py
@@ -0,0 +1,45 @@
+print(5 + 2)
+name = "Oluyosola"
+print(name)
+x = 22
+y = 14
+z = x + y
+print(z)
+# Oluyosola is the best teacher in the world
+print()
+
+# four different variants of "Two Little Blackbirds" Poem ref https://en.wikipedia.org/wiki/Two_Little_Dickie_Birds
+location = "hill"
+character1 = "Jack"
+character2 = "Jill"
+print("Two little blackbirds sitting on a " + location)
+print("One named " + character1 + " and one named " + character2)
+print("Fly away " + character1 + ", fly away " + character2)
+print("Come back " + character1 + ", fly away " + character2)
+
+print()
+location = "sky"
+character1 = "Low"
+character2 = "High"
+print("Two little blackbirds flying in the " + location)
+print("One named " + character1 + " and one named " + character2)
+print("Fly away " + character1 + ", fly away " + character2)
+print("Come back " + character1 + ", fly away " + character2)
+
+print()
+location = "pole"
+character1 = "Fast"
+character2 = "Slow"
+print("Two little blackbirds sitting on a " + location)
+print("One named " + character1 + " and one named " + character2)
+print("Fly away " + character1 + ", fly away " + character2)
+print("Come back " + character1 + ", fly away " + character2)
+
+print()
+location = "gate"
+character1 = "Early"
+character2 = "Late"
+print("Two little blackbirds sitting on a " + location)
+print("One named " + character1 + " and one named " + character2)
+print("Fly away " + character1 + ", fly away " + character2)
+print("Come back " + character1 + ", fly away " + character2)
---
+++ ten-green-bottles.py
@@ -0,0 +1,86 @@
+# Poem: 10 green bottles hanging on the wall Source:https://allnurseryrhymes.com/ten-green-bottles/
+print("Ten Green Bottles Hanging on the Wall")
+print()
+
+number = 10
+line1_2 = " green bottles hanging on the wall"
+line3 = "and if one green bottle should accidentally fall,"
+line4_1 = "there’ll be "
+line4_2 = " green bottles hanging on the wall."
+
+# Numbers in the poem should all be in words
+# Used list to convert numbers to word
+
+
+num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eighth", "nine", "ten"]
+
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+print()
+number = 9
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+print()
+number = 8
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+print()
+number = 7
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+print()
+number = 6
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+print()
+number = 5
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+print()
+number = 4
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+print()
+number = 3
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+print()
+number = 2
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+print()
+number = 1
+print(num[number] + line1_2)
+print(num[number] + line1_2)
+print(line3)
+print(line4_1 + num[number-1] + line4_2)
+
+name = "my name is omotola"
+print(type(name))
