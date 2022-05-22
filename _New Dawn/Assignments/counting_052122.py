a6535e7d7ad4666a9329c608f5efd103b90cb9d5
Toulah <ajao@uwindsor.ca>
2022-05-22 00:55:08 -0400

    conversion of integer number to words done!

--- _New Dawn/Assignments/counting_052122.py
+++ _New Dawn/Assignments/counting_052122.py
@@ -35,10 +35,27 @@ print(max(total_precipitation))
 print(total_precipitation.index(25.9))
 
 # Using dictionary to convert integer number to words
-num = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine",
-       10: "ten", 11: "eleven", 12: "twelve"}
+nums = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine",
+        10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen",
+        17: "seventeen", 18: "eighteen", 19: "nineteen", 20: "twenty", 30: "thirty", 40: "forty", 50: "fifty",
+        60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety", 100: "one hundred", 200: "two hundred",
+        300: "three hundred", 400: "four hundred", 500: "five hundred", 600: "six hundred", 700: "seven hundred",
+        800: "eight hundred", 900: "nine hundred", 1000: "one thousand"}
 
-print(num.get(8))
+# zero_precipitation = 36
+
+if zero_precipitation in nums:
+    ans_1 = nums.get(zero_precipitation)
+    print(ans_1)
+else:
+    units = zero_precipitation % 10
+    units = nums.get(units)
+    tens = zero_precipitation//10
+    tens = tens * 10
+    tens = nums.get(tens)
+    ans_1 = tens + " " + units
+    print(ans_1)
+
 
 print("")
 print(type(climatic_data))
@@ -46,5 +63,4 @@ print(type(precipitation))
 print(type(total_precipitation))
 print(type(rows))
 print(type(zero_precipitation))
-print(type(num))
-
+print(type(nums))
