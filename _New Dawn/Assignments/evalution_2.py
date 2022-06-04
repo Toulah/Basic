9fc3b56cec21f28e5f6ec596d7ed8b6ebe067ad6
Toulah <ajao@uwindsor.ca>
2022-06-04 08:16:42 -0400

    today's class

---
+++ _New Dawn/Assignments/evalution_2.py
@@ -0,0 +1,21 @@
+# from function_060422 import sub
+#
+# # specimen = 3025
+# #
+# # spec = specimen % 1000
+# # print(spec)
+# #
+# # spec_2 = (specimen - (specimen // 100) * 100)
+# # print(spec_2)
+# #
+# # units = specimen % 1000
+# # print(units)
+# #
+# # temperature_input = 45
+# # c = ((int(temperature_input)) - 32) * 5 // 9
+# # print(c)
+#
+# sub(43, 23)
+
+from function_060422 import fall_dist
+fall_dist(3)
---
+++ _New Dawn/Assignments/function_060422.py
@@ -0,0 +1,39 @@
+# # num_1 = input("first number: ")
+# # num_2 = input("second number: ")
+# # addition = int(num_1) + int(num_2)
+# # print(addition)
+# #
+# # print("")
+# # rat = 10
+# # cat = 6
+# # subtraction = rat - cat
+# # print(subtraction)
+#
+#
+# def sub(num_3, num_4):
+#     result = num_3 - num_4
+#     return result
+#
+#
+# sub(6, 5)
+#
+# def myName(name):
+#     print("my name is " + name)
+#
+# myName("tola")
+# myName("yosola")
+#
+# # def hello():
+# #     print("Hello world")
+# #
+# # hello()
+
+
+def plus_two(n):
+    out = n + 2
+    print(out)
+
+
+def fall_dist(t, g=9.81):
+    d = 0.5 * g * t**2
+    print(d)
